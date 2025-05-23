ed13244c6433b43bd724d1d4cd2c92307911132c
Maurício Gardini <accounts@mauriciogardini.com>
2025-05-23 18:56:47 +0000

    Rename Value to ValueDeclaration

--- packages/python/test/classes.test.tsx
+++ packages/python/test/classes.test.tsx
@@ -103,8 +103,8 @@ describe("Python Class", () => {
           <py.ClassDeclaration name="A" />
           <py.ClassDeclaration name="B">
             <List hardline>
-              <py.Variable name="bar" type={refkey("A")} omitNone />
-              <py.Variable name="foo" type="str" omitNone />
+              <py.VariableDeclaration name="bar" type={refkey("A")} omitNone />
+              <py.VariableDeclaration name="foo" type="str" omitNone />
             </List>
           </py.ClassDeclaration>
         </py.SourceFile>
