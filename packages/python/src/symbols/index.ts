3c09d9afbffc1d25ff973c576981cd3358a548d1
Maurício Gardini <accounts@mauriciogardini.com>
2025-06-09 20:25:41 +0000

    Fix

--- packages/python/src/symbols/index.ts
+++ packages/python/src/symbols/index.ts
@@ -3,4 +3,3 @@ export * from "./scopes.js";
 export * from "./python-member-scope.js";
 export * from "./python-module-scope.js";
 export * from "./python-output-symbol.js";
-export * from "./python-package-scope.js";
