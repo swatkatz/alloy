3ba55c96de3829216a2269b3b27f3bfb4d5ddc5a
Maurício Gardini <accounts@mauriciogardini.com>
2025-06-09 21:31:31 +0000

    Fix

--- packages/python/test/classes.test.tsx
+++ packages/python/test/classes.test.tsx
@@ -5,33 +5,33 @@ import * as py from "../src/components/index.js";
 import { assertFileContents, toSourceText } from "./utils.jsx";
 
 describe("Python Class", () => {
-  // it("renders a class with no body as 'pass'", () => {
-  //   const result = toSourceText(
-  //     <>
-  //       <py.SourceFile path="test.py">
-  //         <py.ClassDeclaration name="Foo" />
-  //       </py.SourceFile>
-  //     </>
-  //   );
-  //   expect(result).toRenderTo(d`
-  //     class Foo:
-  //       pass
-  //   `);
-  // });
+  it("renders a class with no body as 'pass'", () => {
+    const result = toSourceText(
+      <>
+        <py.SourceFile path="test.py">
+          <py.ClassDeclaration name="Foo" />
+        </py.SourceFile>
+      </>
+    );
+    expect(result).toRenderTo(d`
+      class Foo:
+        pass
+    `);
+  });
 
-  // it("renders a class with a body", () => {
-  //   const result = toSourceText(
-  //     <>
-  //       <py.SourceFile path="test.py">
-  //         <py.ClassDeclaration name="Bar">print('hi')</py.ClassDeclaration>
-  //       </py.SourceFile>
-  //     </>,
-  //   );
-  //   expect(result).toRenderTo(d`
-  //     class Bar:
-  //       print('hi')
-  //   `);
-  // });
+  it("renders a class with a body", () => {
+    const result = toSourceText(
+      <>
+        <py.SourceFile path="test.py">
+          <py.ClassDeclaration name="Bar">print('hi')</py.ClassDeclaration>
+        </py.SourceFile>
+      </>,
+    );
+    expect(result).toRenderTo(d`
+      class Bar:
+        print('hi')
+    `);
+  });
 
   it("renders a class with base classes", () => {
     const result = render(
@@ -58,70 +58,70 @@ describe("Python Class", () => {
     assertFileContents(result, { "test.py": expected });
   });
 
-  // it("renders a class with base classes and body", () => {
-  //   const result = toSourceText(
-  //     <>
-  //       <py.SourceFile path="test.py">
-  //         <py.ClassDeclaration name="Qux" bases={["Base"]}>
-  //           print('hello')
-  //         </py.ClassDeclaration>
-  //       </py.SourceFile>
-  //     </>,
-  //   );
-  //   expect(result).toRenderTo(d`
-  //     class Qux(Base):
-  //       print('hello')
-  //   `);
-  // });
+  it("renders a class with base classes and body", () => {
+    const result = toSourceText(
+      <>
+        <py.SourceFile path="test.py">
+          <py.ClassDeclaration name="Qux" bases={["Base"]}>
+            print('hello')
+          </py.ClassDeclaration>
+        </py.SourceFile>
+      </>,
+    );
+    expect(result).toRenderTo(d`
+      class Qux(Base):
+        print('hello')
+    `);
+  });
 
-  // it("renders classes across modules with inheritance", () => {
-  //   const result = render(
-  //     <Output>
-  //       <py.SourceFile path="mod1.py">
-  //         <py.ClassDeclaration name="A" />
-  //       </py.SourceFile>
-  //       <py.SourceFile path="mod2.py">
-  //         <py.ClassDeclaration name="B" bases={[refkey("A")]} />
-  //       </py.SourceFile>
-  //     </Output>,
-  //   );
-  //   const mod1Expected = d`
-  //     class A:
-  //       pass
-  //   `;
-  //   const mod2Expected = d`
-  //     from mod1 import A
+  it("renders classes across modules with inheritance", () => {
+    const result = render(
+      <Output>
+        <py.SourceFile path="mod1.py">
+          <py.ClassDeclaration name="A" />
+        </py.SourceFile>
+        <py.SourceFile path="mod2.py">
+          <py.ClassDeclaration name="B" bases={[refkey("A")]} />
+        </py.SourceFile>
+      </Output>,
+    );
+    const mod1Expected = d`
+      class A:
+        pass
+    `;
+    const mod2Expected = d`
+      from mod1 import A
 
-  //     class B(A):
-  //       pass
-  //   `;
-  //   assertFileContents(result, { "mod1.py": mod1Expected });
-  //   assertFileContents(result, { "mod2.py": mod2Expected });
-  // });
+      class B(A):
+        pass
+    `;
+    assertFileContents(result, { "mod1.py": mod1Expected });
+    assertFileContents(result, { "mod2.py": mod2Expected });
+  });
 
-  // it("renders a class with class variables like foo: str, and also bar: A where A is another class", () => {
-  //   const result = render(
-  //     <Output>
-  //       <py.SourceFile path="test.py">
-  //         <py.ClassDeclaration name="A" />
-  //         <hbr />
-  //         <hbr />
-  //         <py.ClassDeclaration name="B">
-  //           <List hardline>
-  //             <py.VariableDeclaration name="bar" type={refkey("A")} omitNone />
-  //             <py.VariableDeclaration name="foo" type="str" omitNone />
-  //           </List>
-  //         </py.ClassDeclaration>
-  //       </py.SourceFile>
-  //     </Output>,
-  //   );
-  //   const expected = d`
-  //     class A:
-  //       pass
-  //     class B:
-  //       bar: A
-  //       foo: str
-  //   `;
-  //   assertFileContents(result, { "test.py": expected });
-  // });
+  it("renders a class with class variables like foo: str, and also bar: A where A is another class", () => {
+    const result = render(
+      <Output>
+        <py.SourceFile path="test.py">
+          <py.ClassDeclaration name="A" />
+          <hbr />
+          <hbr />
+          <py.ClassDeclaration name="B">
+            <List hardline>
+              <py.VariableDeclaration name="bar" type={refkey("A")} omitNone />
+              <py.VariableDeclaration name="foo" type="str" omitNone />
+            </List>
+          </py.ClassDeclaration>
+        </py.SourceFile>
+      </Output>,
+    );
+    const expected = d`
+      class A:
+        pass
+      class B:
+        bar: A
+        foo: str
+    `;
+    assertFileContents(result, { "test.py": expected });
+  });
 });
