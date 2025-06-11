7fd5d0b8999a9bf7527a9e224f0ce31fd790b313
Maurício Gardini <accounts@mauriciogardini.com>
2025-06-11 19:01:56 +0000

    Remove code

--- packages/python/src/symbols/python-module-scope.ts
+++ packages/python/src/symbols/python-module-scope.ts
@@ -1,33 +1,4 @@
-import { Binder, OutputScope, reactive, shallowReactive } from "@alloy-js/core";
-import { PythonOutputSymbol } from "./python-output-symbol.js";
-
-export class ImportedSymbol {
-  local?: PythonOutputSymbol;  // The alias of the target symbol, if it exists
-  target: PythonOutputSymbol;
-
-  constructor(target: PythonOutputSymbol, local?: PythonOutputSymbol) {
-    this.target = target;
-    this.local = local;
-  }
-
-  static from(target: PythonOutputSymbol, local?: PythonOutputSymbol) {
-    return new ImportedSymbol(target, local);
-  }
-}
-
-export interface ImportRecordProps {
-  symbols: Set<ImportedSymbol>;
-  pathAlias?: string; // Alias for the module itself (if importing the whole module)
-  wildcard?: boolean; // If true, use '*'
-}
-
-export type ImportRecords = Map<PythonModuleScope, ImportRecordProps>;
-
-export const ImportRecords = Map as {
-  new (): ImportRecords;
-  new (entries?: readonly (readonly [PythonModuleScope, ImportRecordProps])[] | null): ImportRecords;
-  prototype: Map<PythonModuleScope, ImportRecordProps>;
-};
+import { OutputScope } from "@alloy-js/core";
 
 export class PythonModuleScope extends OutputScope {
   get kind() {
