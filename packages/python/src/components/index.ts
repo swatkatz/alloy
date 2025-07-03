82f03cff7004d3117e3cacf9d1ee6ef740ca05ef
Maurício Gardini <accounts@mauriciogardini.com>
2025-07-03 18:18:35 +0000

    Remove ObjectExpression

--- packages/python/src/components/index.ts
+++ packages/python/src/components/index.ts
@@ -9,7 +9,6 @@ export * from "./FunctionDeclaration.js";
 export * from "./ImportStatement.js";
 export * from "./MemberExpression.js";
 export * from "./NewExpression.js";
-export * from "./ObjectExpression.js";
 export * from "./PropertyName.js";
 export * from "./Reference.js";
 export * from "./SourceFile.js";
