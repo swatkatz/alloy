e80f5084426e1ae2dcbe494058c8a8f9e292910a
Maurício Gardini <accounts@mauriciogardini.com>
2025-07-16 17:18:38 +0000

    Remove type from Reference

--- packages/python/src/components/Reference.tsx
+++ packages/python/src/components/Reference.tsx
@@ -3,14 +3,6 @@ import { ref } from "../symbols/index.js";
 
 export interface ReferenceProps {
   refkey: Refkey;
-
-  /**
-   * Whether this is a reference to a type.
-   *
-   * @remarks
-   * This affect things like import where `type` keyword might be used.
-   */
-  type?: boolean;
 }
 
 /**
@@ -19,9 +11,8 @@ export interface ReferenceProps {
  * @remarks
  * This component is used to render references to symbols in Python code.
  * It takes a `refkey` prop which is the key of the symbol to reference.
- * The `type` prop indicates whether this is a reference to a type.
  */
-export function Reference({ refkey, type }: ReferenceProps) {
+export function Reference({ refkey }: ReferenceProps) {
   const reference = ref(refkey);
   const symbolRef = computed(() => reference()[1]);
 
